b5374fdebc2e249579fca3b753b277ecf6db8371
sa <as>
2023-07-23 23:00:56 +0300

    end work

--- Files/task38.py
+++ Files/task38.py
@@ -1,28 +1,36 @@
 
 def change(temp):
-    print('Если вы хотите изменить свои данные на актуальные - введите 1')
-    print('Если вы хотите удалить данные - введите 2')
-    num = input('Ваш выбор')
-    if num == 1:
-        n = str(input('Введите ФИО и старый номер телефона которые хотите поменять: '))
-        x = str(input('Введите полностью актуальные данные (ФИО и номер): '))
-        new_list = ''
-        for i in temp:
-            if i == n:
-                new_list = new_list + x
-            else:
-                new_list = new_list + i + ','
-        new_list = list(new_list.split(','))
-        print(new_list)
-        return new_list
-    elif num == 2:
-        n = input('Введите ФИО и старый номер телефона которые хотите удалить: ')
-        
-
-        
+    n = str(input('Введите ФИО и старый номер телефона которые хотите поменять: '))
+    x = str(input('Введите полностью актуальные данные (ФИО и номер): \n'))
+    new_list = ''
+    for i in temp:
+        if i == n:
+            new_list = new_list + x + ','
+        else:
+            new_list = new_list + i + ','
+    new_list = list(new_list.split(','))
+    new_list.pop(-1)
+    print(new_list)
+    return new_list
 
 
+def delete(temp):
+    n = str(input('Введите ФИО и старый номер телефона которые хотите удалить: '))
+    for i in temp:
+        if i == n:
+            index = temp.index(n)
+            temp.pop(index)
+    return temp
+    
+    
 
+def add(temp):
+
+     print('Введите ФИО и номер который хотите добавить в справиочник')
+     n = str(input('Ввод:\n'))
+     temp.append(n)
+     return temp
+    
 
 
 
@@ -30,7 +38,7 @@ def print_info(temp):
     print(temp)
     str1 = ''
     for i in temp:
-            str1 = str1 +(i+ '\n')
+            str1 = str1 + (i+ '\n')
     print(str1)
     
 
@@ -46,9 +54,12 @@ def read_file():
         return res_str
         
     
-
-def write_file():
-    pass
+def write(data):
+    with open('numbers.txt', 'w') as file:
+        for i in data:
+            print(i)
+            file.write(i + '\n')
+    
 
 
 def menu():
@@ -56,17 +67,26 @@ def menu():
     print(data)
     while True:
         print('Выберите действие')
+        print('5 - Перезаписать справочник')
+        print('4 - добавить данные')
+        print('3 - удалить данные')
         print('2 - заменить номер телефона, фамилии, и тд')
         print('1 - ввыести инфо на экран')
         print('0 выход из программы')
-        n = int(input('Введите ваш выбор: '))
+        n = int(input('Введите ваш выбор: \n'))
+
         if n == 0:
             break
         elif n == 1:
             print_info(data)
         elif n == 2:
             data = change(data)
-            
+        elif n == 3:
+            data = delete(data)
+        elif n == 4:
+            data = add(data)
+        elif n == 5:
+            write(data)
 
 
 if __name__ == '__main__':
@@ -75,3 +95,4 @@ if __name__ == '__main__':
 
 # my_st = "Я\nучу; язык,программирования\nPython"
 # print(re.split(";|,|\n", my_st))
+# data = list(map(int, data.split ()))
